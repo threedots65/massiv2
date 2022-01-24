43d2ce6528ac0b8ae477bc07263128dd06777d9c
threedots65 <98290096+threedots65@users.noreply.github.com>
2022-01-24 04:17:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int max;
+            Random r = new Random();
+            int[,] arr = new int[3, 3] 
+            { {r.Next(0,100), r.Next(0, 100), r.Next(0, 100) },
+            { r.Next(0, 100), r.Next(0, 100), r.Next(0, 100) },
+            { r.Next(0, 100), r.Next(0, 100), r.Next(0, 100) }};
+
+            for (int i = 0;i<3;i++)
+            {
+                for (int j = 0;j<3;j++)
+                {
+                    Console.Write(arr[i,j]+"\t");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine();
+            for (int i = 0; i<3;i++)
+            {
+                max = 0;
+                for (int j = 0;j<3;j++)
+                {
+                    if (max<arr[i,j])
+                     max=arr[i,j];
+                }
+                Console.WriteLine(max);
+            }
+            Console.ReadKey();
+
+
+            
+
+
+        }
+    }
+}
